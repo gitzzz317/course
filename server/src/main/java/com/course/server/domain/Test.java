f55e9b72ccc74ae1316f8a824df1181dab5f07f9
hzw001 <2832572746@qq.com>
2022-08-15 02:09:57 +0800

    1、解决mapper重复生成问题 <plugin type="org.mybatis.generator.plugins.UnmergeableXmlMappersPlugin" />

--- server/src/main/java/com/course/server/domain/Test.java
+++ server/src/main/java/com/course/server/domain/Test.java
@@ -1,19 +1,9 @@
 package com.course.server.domain;
 
-import org.springframework.stereotype.Repository;
-
-@Repository
 public class Test {
     private String id;
+
     private String name;
-
-    public Test() {
-    }
-
-    public Test(String id, String name) {
-        this.id = id;
-        this.name = name;
-    }
 
     public String getId() {
         return id;
@@ -29,5 +19,17 @@ public class Test {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", name=").append(name);
+        sb.append("]");
+        return sb.toString();
     }
 }
--- server/src/main/java/com/course/server/mapper/TestMapper.java
+++ server/src/main/java/com/course/server/mapper/TestMapper.java
@@ -1,15 +1,30 @@
 package com.course.server.mapper;
 
 import com.course.server.domain.Test;
-import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Repository;
-
+import com.course.server.domain.TestExample;
 import java.util.List;
+import org.apache.ibatis.annotations.Param;
 
-@Repository
-@Mapper
 public interface TestMapper {
+    long countByExample(TestExample example);
 
-    List<Test> list();
+    int deleteByExample(TestExample example);
 
+    int deleteByPrimaryKey(String id);
+
+    int insert(Test record);
+
+    int insertSelective(Test record);
+
+    List<Test> selectByExample(TestExample example);
+
+    Test selectByPrimaryKey(String id);
+
+    int updateByExampleSelective(@Param("record") Test record, @Param("example") TestExample example);
+
+    int updateByExample(@Param("record") Test record, @Param("example") TestExample example);
+
+    int updateByPrimaryKeySelective(Test record);
+
+    int updateByPrimaryKey(Test record);
 }
