cbf33f4e8f1a45b66b34446e963f7bdd0e4835ff
hzw001 <2832572746@qq.com>
2022-08-16 23:04:37 +0800

    1、组件welcome子组件，组件admin/welcome子路由 router-view和子路由配合使用 注：子路由的配置开头不用“/”开头

---
+++ admin/src/views/admin/welcome.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>
+            你好，欢迎加入控台
+        </h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "welcome"
+    }
+</script>
+
+<style scoped>
+
+</style>
