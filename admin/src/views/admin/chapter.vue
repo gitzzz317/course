2fe75e4f3e77dc0b4660a8fc991bc8332c3d14a9
hzw001 <2832572746@qq.com>
2022-08-18 11:20:37 +0800

    1、增加新增大章功能，前端代码开发 2、eslint关闭no-unused-vars校验

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -1,6 +1,12 @@
 <template>
     <div>
         <p>
+
+            <button v-on:click="add()" class="btn btn-white btn-default btn-round">
+                <i class="ace-icon fa fa-edit"></i>
+                新增
+            </button>
+
             <button @click="list(1)" class="btn btn-white btn-default btn-round">
                 <i class="ace-icon fa fa-times red2"></i>
                 刷新
@@ -79,6 +85,37 @@
             </tr>
             </tbody>
         </table>
+
+        <div id="form-modal" class="modal fade" tabindex="-1" role="dialog">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title">表单</h4>
+                    </div>
+                    <div class="modal-body">
+                        <form class="form-horizontal">
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">名称</label>
+                                <div class="col-sm-10">
+                                    <input class="form-control" placeholder="名称">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">课程</label>
+                                <div class="col-sm-10">
+                                    <input class="form-control" placeholder="课程ID">
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>
+                        <button type="button" class="btn btn-primary">保存</button>
+                    </div>
+                </div><!-- /.modal-content -->
+            </div><!-- /.modal-dialog -->
+        </div><!-- /.modal -->
     </div>
 
 
@@ -105,9 +142,17 @@
         },
         methods: {
             /**
+             * 点击【新增】
+             */
+            add() {
+                let _this = this;
+                // _this.chapter = {};
+                $("#form-modal").modal("show");
+            },
+            /**
              * 列表查询
              */
-            list: function (page) {
+            list(page) {
                 let _this = this;
                 _this.$ajax.post('http://127.0.0.1:9000/business/admin/chapter/list',{
                         page: page,
