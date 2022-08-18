25b612f1278f1e54f5283fefbfd79839c4f2bdb3
hzw001 <2832572746@qq.com>
2022-08-18 17:05:37 +0800

    1、增加后端的校验工具类ValidatorUtil 2、增加统一异常处理，ControllerExceptionHandler,关键字@ControllerAdvice

--- business/src/main/java/com/course/business/controller/admin/ChapterController.java
+++ business/src/main/java/com/course/business/controller/admin/ChapterController.java
@@ -5,6 +5,7 @@ import com.course.server.dto.PageDto;
 import com.course.server.dto.ResponseDto;
 import com.course.server.service.ChapterService;
 import com.course.server.util.UuidUtil;
+import com.course.server.util.ValidatorUtil;
 import org.apache.ibatis.annotations.Delete;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,6 +34,12 @@ public class ChapterController {
     @PostMapping("/save")
     public ResponseDto save(@RequestBody ChapterDto chapterDto ){
         LOG.info("ChapterDto:{}", chapterDto);
+        // 保存校验
+        ValidatorUtil.require(chapterDto.getName(), "名称");
+        ValidatorUtil.require(chapterDto.getCourseId(), "课程ID");
+        ValidatorUtil.length(chapterDto.getCourseId(), "课程ID", 1, 8);
+
+
         chapterService.save(chapterDto);
         ResponseDto responseDto = new ResponseDto();
         responseDto.setContent(chapterDto);
