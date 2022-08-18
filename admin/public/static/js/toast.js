ef3fe20f4a224b295c9aa2209bf50899e12cfab1
hzw001 <2832572746@qq.com>
2022-08-18 16:06:06 +0800

    1、制作toast组件，内部用sweetalert2实现

---
+++ admin/public/static/js/toast.js
@@ -0,0 +1,64 @@
+const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.addEventListener('mouseenter', Swal.stopTimer)
+    toast.addEventListener('mouseleave', Swal.resumeTimer)
+  }
+})
+
+toast = {
+  success: function (message) {
+    Toast.fire({
+      icon: 'success',
+      title:message
+    })
+  },
+  error: function (message) {
+    Toast.fire({
+      icon: 'error',
+      title:message
+    })
+  },
+  warning: function (message) {
+    Toast.fire({
+      icon: 'warning',
+      title:message
+    })
+  },
+}
+
+// Toast = {
+//   success: function (message) {
+//     Swal.fire({
+//       position: 'top-end',
+//       icon: 'success',
+//       title: message,
+//       showConfirmButton: false,
+//       timer: 3000
+//     })
+//   },
+//
+//   error: function (message) {
+//     Swal.fire({
+//       position: 'top-end',
+//       icon: 'error',
+//       title: message,
+//       showConfirmButton: false,
+//       timer: 3000
+//     })
+//   },
+//
+//   warning: function (message) {
+//     Swal.fire({
+//       position: 'top-end',
+//       icon: 'warning',
+//       title: message,
+//       showConfirmButton: false,
+//       timer: 3000
+//     })
+//   }
+// };
