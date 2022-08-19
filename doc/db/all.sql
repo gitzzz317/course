f915db2ed0e897c9ed60f4059a4430af69b3b053
hzw001 <2832572746@qq.com>
2022-08-19 20:52:01 +0800

    1、增加课程内容表设计，与代码生成

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -139,7 +139,7 @@ create table `course_content` (
   `id` char(8) not null default '' comment '课程id',
   `content` mediumtext not null comment '课程内容',
   primary key (`id`)
-) engine=innodb default charset=utf8mb4 comment='课程内容';
+) engine=innodb default charset=utf8 comment='课程内容';
 
 -- 课程内容文件
 drop table if exists `course_content_file`;
