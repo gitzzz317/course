584ba7cda03e60d0fd06ae61dcdb497a3c0ef92c
hzw001 <2832572746@qq.com>
2022-08-25 21:09:48 +0800

    1、大文件断点续传与极速秒传：设置分片的文件名，以分片索引结尾

--- file/src/main/java/com/course/file/controller/admin/UploadController.java
+++ file/src/main/java/com/course/file/controller/admin/UploadController.java
@@ -52,7 +52,15 @@ public class UploadController {
             fullDir.mkdir();
         }
 
-        String path = dir + File.separator + key + "." + suffix;
+//        String path = dir + File.separator + key + "." + suffix + fileDto.getShardIndex();
+        String path = new StringBuffer(dir)
+                .append(File.separator)
+                .append(key)
+                .append(".")
+                .append(suffix)
+                .append(".")
+                .append(fileDto.getShardIndex())
+                .toString();
 
         String fullPath = FILE_PATH + path;
 
