b044421b044d4f250b4492553fff1931abc3d7c7
hzw001 <2832572746@qq.com>
2022-10-09 23:53:52 +0800

    1、通用权限管理：增加角色用户关联表，生成持久层和服务端代码

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -257,7 +257,7 @@ create table `role_user` (
   `role_id` char(8) not null comment '角色|id',
   `user_id` char(8) not null comment '用户|id',
   primary key (`id`)
-) engine=innodb default charset=utf8mb4 comment='角色用户关联';
+) engine=innodb default charset=utf8 comment='角色用户关联';
 
 insert into `role_user` values ('00000000', '00000000', '10000000');
 
