17059240f98efd08b8f7a42919154318c05bd044
hzw001 <2832572746@qq.com>
2022-10-09 17:04:36 +0800

    1、用户管理与登录：生成登录token并存储到redis中;退出登录时删除token 2、日志优化

--- system/src/main/java/com/course/system/controller/admin/KaptchaController.java
+++ system/src/main/java/com/course/system/controller/admin/KaptchaController.java
@@ -40,7 +40,7 @@ public class KaptchaController {
             String createText = defaultKaptcha.createText();
 
             // 将生成的验证码放入会话缓存中，后续验证的时候用到
-             request.getSession().setAttribute(imageCodeToken, createText);
+//             request.getSession().setAttribute(imageCodeToken, createText);
             // 将生成的验证码放入redis缓存中，后续验证的时候用到
             redisTemplate.opsForValue().set(imageCodeToken, createText, 300, TimeUnit.SECONDS);
 
