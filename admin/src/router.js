362fa5edfdf4351c81c080e9f48bbb72a0793c0c
hzw001 <2832572746@qq.com>
2022-10-09 17:12:35 +0800

    1、用户管理与登录：基于Vue路由的登录拦截

--- admin/src/router.js
+++ admin/src/router.js
@@ -31,6 +31,9 @@ export default new Router({
         path: "/",
         name: "admin",
         component: Admin,
+        meta: {
+            loginRequire: true
+        },
         children: [{
             path: "welcome",
             name: "welcome",
